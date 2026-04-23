4fcc87ce2db63c1544270ac25a4049a224012476
nateelive <natee.live@gmail.com>
2026-04-23 12:21:37 +0700

    update pm2 script to windows

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'import-xlsx-tranout-dev',
+      script: 'npm',
+      args: 'run dev',
+      shell: true,
+      watch: false,
+    },
+    {
+      name: 'import-xlsx-tranout',
+      script: 'node',
+      args: '.next/standalone/server.js',
+      watch: false,
+    },
+  ],
+}
