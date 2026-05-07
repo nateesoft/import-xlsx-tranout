4691311502547739d3237badd7d78a67b5128812
nateelive <natee.live@gmail.com>
2026-05-07 15:54:28 +0700

    update ecosystem for pm2 start service

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,10 +1,21 @@
 module.exports = {
   apps: [
     {
+      name: 'import-xlsx-tranout',
+      script: 'server.js',
+      cwd: __dirname,
+      watch: false,
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        HOSTNAME: '0.0.0.0',
+      },
+    },
+    {
       name: 'import-xlsx-tranout-dev',
       script: 'cmd',
       args: '/c npm run dev',
       watch: false,
-    }
+    },
   ],
 }
