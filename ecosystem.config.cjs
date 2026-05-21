25460dcf2b5394d4108db615b1d49e17460e9ea1
nateelive <natee.live@gmail.com>
2026-05-22 06:31:51 +0700

    update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
       script: '.next/standalone/server.js',
       cwd: __dirname,
       watch: false,
-      env_production: {
+      env: {
         NODE_ENV: 'production',
         PORT: 3000,
         HOSTNAME: '0.0.0.0',
